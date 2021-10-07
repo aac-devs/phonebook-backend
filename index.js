4b9a56867dd7cc012d602cd7ef1351eaa7e8610a
Andrés <aac.devs@gmail.com>
2021-10-07 07:30:10 -0300

    cors added

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import morgan from 'morgan';
 import config from './morganConfig.js';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-
 app.use(morgan(config));
 
 let persons = [
