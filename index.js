b93e473cb591d4aa27c049d732fea3c1cf8bfe42
Andrés <aac.devs@gmail.com>
2021-10-08 11:48:10 -0300

    part3 exercise3.15 finished

--- index.js
+++ index.js
@@ -10,6 +10,37 @@ app.use(express.json());
 app.use(morgan(config));
 app.use(express.static('build'));
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+// Mongo
+app.post('/api/persons', (req, res) => {
+  if (!req.body?.name)
+    return res.status(400).json({
+      error: 'name missing',
+    });
+  if (!req.body?.number)
+    return res.status(400).json({
+      error: 'number missing',
+    });
+  const { name, number } = req.body;
+
+  const person = new Person({ name, number });
+  person.save().then((personSaved) => {
+    res.json(personSaved);
+  });
+});
+
+// Mongo
 app.get('/api/persons', (req, res) => {
   Person.find({}).then((persons) => {
     res.json(persons);
@@ -33,28 +64,17 @@ app.get('/api/persons/:id', (req, res) => {
   }
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const { id } = req.params;
-  persons = persons.filter((p) => p.id !== +id);
-  res.status(204).end();
+// Mongo
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.post('/api/persons', (req, res) => {
-  if (!req.body?.name)
-    return res.status(400).json({
-      error: 'name missing',
-    });
-  if (!req.body?.number)
-    return res.status(400).json({
-      error: 'number missing',
-    });
-  const { name, number } = req.body;
-
-  const person = new Person({ name, number });
-  person.save().then((personSaved) => {
-    res.json(personSaved);
-  });
-});
+app.use(errorHandler);
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
