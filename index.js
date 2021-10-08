f756bf9026a4ac1c1f5e40751df2189a9f8610de
Andrés <aac.devs@gmail.com>
2021-10-08 12:21:31 -0300

    part3 exercise3.16-17-18 finished

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
-// Mongo
+// CREATE
 app.post('/api/persons', (req, res) => {
   if (!req.body?.name)
     return res.status(400).json({
@@ -33,42 +33,59 @@ app.post('/api/persons', (req, res) => {
       error: 'number missing',
     });
   const { name, number } = req.body;
-
   const person = new Person({ name, number });
   person.save().then((personSaved) => {
     res.json(personSaved);
   });
 });
 
-// Mongo
+// GET ALL
 app.get('/api/persons', (req, res) => {
   Person.find({}).then((persons) => {
     res.json(persons);
   });
 });
 
+// GET INFO
 app.get('/info', (req, res) => {
-  const msg = `<p>Phonebook has info for ${
-    persons.length
-  } people</p><p>${new Date()}</p>`;
-  res.send(msg);
+  Person.find({}).then((persons) => {
+    const msg = `<p>Phonebook has info for ${
+      persons.length
+    } people</p><p>${new Date()}</p>`;
+    res.send(msg);
+  });
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  const { id } = req.params;
-  const person = persons.find((p) => p.id === +id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).send();
-  }
+// GET ONE
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).send();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-// Mongo
+// DELETE
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then((result) => {
       res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+// PUT
+app.put('/api/persons/:id', (req, res, next) => {
+  const person = {
+    number: req.body.number,
+  };
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
     })
     .catch((error) => next(error));
 });
